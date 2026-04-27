d31761aeae58687438985c8c7eb29101ca11368e
Natalia Filtanovych <natashe4ka21092016@gmail.com>
2026-04-27 19:27:53 +0300

    add components VoteStas and Notification

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,8 @@ import CafeInfo from "../CafeInfo/CafeInfo";
 import css from "./App.module.css";
 import type { VoteType } from "../../types/votes";
 import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
 
 const App = () => {
   const [votes, setVotes] = useState({
@@ -16,13 +18,35 @@ const App = () => {
   };
 
   const resetVotes = () => {
-    handleVote(null);
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
   };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
 
   return (
     <div className={css.app}>
       <CafeInfo />
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
+      {totalVotes > 0 && (
+        <VoteStats
+          votes={votes}
+          totalVotes={totalVotes}
+          positiveRate={positiveRate}
+        />
+      )}
+      {totalVotes === 0 && <Notification />}
     </div>
   );
 };
